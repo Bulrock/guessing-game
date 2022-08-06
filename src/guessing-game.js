7fa7be229f9d152741aa4a8ca27fb7e4e45c75fe
Bulrock <maksim.volozhinskij@gmail.com>
2022-08-06 16:49:36 +0300

    feat: implement GuessingGame class

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,29 @@
 class GuessingGame {
-    constructor() {}
 
+    constructor() {};
+    
     setRange(min, max) {
-
-    }
+        this.minBorder = min;
+        this.maxBorder = max;
+        console.log(this.minBorder, this.maxBorder)
+    };
 
     guess() {
-
-    }
+        console.log(Math.ceil((this.minBorder+this.maxBorder)/2));
+        return (Math.ceil((this.minBorder+this.maxBorder)/2));
+    };
 
     lower() {
-
-    }
+        this.maxBorder = this.guess();
+        console.log(`min border: ${this.minBorder}`, `max border: ${this.maxBorder}`);
+        return this.maxBorder;
+    };
 
     greater() {
-
-    }
+        this.minBorder = this.guess();
+        console.log(`min border: ${this.minBorder}`, `max border: ${this.maxBorder}`);
+        return this.minBorder;
+    };
 }
 
 module.exports = GuessingGame;
